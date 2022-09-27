8de9f6c9f9122a0c5c81662ecc560bd4ccb14525
kjminn <kjminn2001@gmail.com>
2022-09-27 12:56:17 +0900

    BMI

---
+++ app/src/main/java/kr/ac/yuhan/minn/kotlintest2/CalcActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.yuhan.minn.kotlintest2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calc)
+    }
+}
---
+++ app/src/main/java/kr/ac/yuhan/minn/kotlintest2/MainActivity.kt
@@ -0,0 +1,26 @@
+package kr.ac.yuhan.minn.kotlintest2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kr.ac.yuhan.minn.kotlintest2.databinding.ActivityMainBinding
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.btnResult.setOnClickListener(btnListener)
+    }
+
+    val btnListener = View.OnClickListener {
+//        var w = binding.editWeight.text.toString().toInt();
+        var weight:Double =binding.editWeight.text.toString().toDouble()
+        var height:Double = binding.editHeight.text.toString().toDouble()
+        var bmi:Double = weight / (height/100).pow(2)
+        binding.textResult.text = ""+bmi
+    }
+}
