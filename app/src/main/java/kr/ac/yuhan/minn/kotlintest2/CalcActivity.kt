1e497c574878dd95466d82e6a517e95e7036369a
kjminn <kjminn2001@gmail.com>
2022-09-27 13:39:50 +0900

    사칙연산기

--- app/src/main/java/kr/ac/yuhan/minn/kotlintest2/CalcActivity.kt
+++ app/src/main/java/kr/ac/yuhan/minn/kotlintest2/CalcActivity.kt
@@ -2,10 +2,39 @@ package kr.ac.yuhan.minn.kotlintest2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kr.ac.yuhan.minn.kotlintest2.databinding.ActivityCalcBinding
+import kr.ac.yuhan.minn.kotlintest2.databinding.ActivityMainBinding
 
 class CalcActivity : AppCompatActivity() {
+    val b by lazy {
+        ActivityCalcBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calc)
+        setContentView(b.root)
+        var btnArr = arrayOf<Button>(b.btnPlus, b.btnMinus, b.btnMulti, b.btnDvide)
+        var i=0;
+        for (btn in btnArr){
+            btnArr[i++].setOnClickListener(btnListener)
+        }
+
+    }
+
+    val btnListener = View.OnClickListener{
+        var eStr1 = b.edit1.text.toString()
+        var eStr2 = b.edit2.text.toString()
+        var num1 = eStr1.toInt();
+        var num2 = eStr2.toInt();
+        var result = 0;
+        when(it.id){
+            R.id.btnPlus -> result = num1 + num2
+            R.id.btnMinus -> result = num1 - num2
+            R.id.btnMulti -> result = num1 * num2
+            R.id.btnDvide -> result = num1 / num2
+        }
+        b.textResult.setText(R.string.text_result)
+        b.textResult.append(result.toString())
     }
 }
